d7a1d8fddeac7cc78f770833555692f8b6696513
infoambassador <mlewis0260@gmail.com>
2017-09-08 15:05:09 -0500

    Added functionality to populate invoice from a spreadsheet.

--- script.py
+++ script.py
@@ -1,39 +1,51 @@
 from __future__ import print_function
 import os
+import xlrd
 from mailmerge import MailMerge
 from datetime import date
 
 template = 'templates/invoice_template.docx'
+excel_sheet = xlrd.open_workbook('data_source/invoice_data.xlsx').sheet_by_index(0)
 
 document = MailMerge(template)
 
-invoice_info = {
-    'Name': 'John Smith',
-    'StreetAddress': '5555 FakeAddress Drive',
-    'CityStateZip':'Austin, TX 78749',
-    'InvoiceDate': '{:%b %d %Y}'.format(date.today()),
-    'PayeeName': 'Kelly Smith',
-    'PayeeStreetAddress': '5555 FakeAddress Drive',
-    'PayeeCityStateZip': 'Austin, TX 78749',
-    'TotalBilled': '$4000.00'
-}
-
-WorkEvent = {
-        'Title': 'Texas Pathways Project, Institute #2',
-        'Duration': 3,
-        'Description': 'Assisted in lots of stuff.'
+def get_invoice_info(excel_sheet):
+    invoice_info = {
+        'Name': excel_sheet.cell(1,1).value,
+        'StreetAddress': excel_sheet.cell(2,1).value,
+        'CityStateZip': excel_sheet.cell(3,1).value,
+        'InvoiceDate': '{:%b %d %Y}'.format(date.today()),
+        'PayerName': excel_sheet.cell(6,1).value,
+        'PayerStreetAddress': excel_sheet.cell(7,1).value,
+        'PayerCityStateZip': excel_sheet.cell(8,1).value,
+        'TotalBilled': '$4000.00'
     }
+    return invoice_info
 
+def get_work_events(excel_sheet):
+    work_events = []
+    total_billed = 0
+    for i in range(11,excel_sheet.nrows):
+        row_values = excel_sheet.row_values(i)
+        year,month,day = xlrd.xldate_as_tuple(row_values[0],0)[:3]
+        title = row_values[1]
+        duration = row_values[2]
+        total_billed += duration*150
+        description = row_values[3]
+        work_event = {
+            'Date': '{}/{}/{}'.format(month,day,year),
+            'WorkTitle': title,
+            'Duration': str(duration),
+            'WorkDescription': description,
+            'AmountBilled': '${}.00'.format(str(int(duration)*150))
+        }
+        work_events.append(work_event)
+    return work_events, total_billed
 
-work_history = [
-    {
-        'WorkDescription':WorkEvent['Title'],
-        'Duration':str(WorkEvent['Duration']),
-        'WorkDetails':WorkEvent['Description'],
-        'AmountBilled': '${}.00'.format(str(WorkEvent['Duration']*150))
-    }
-]
+invoice_info = get_invoice_info(excel_sheet)
+work_events, total_billed = get_work_events(excel_sheet)
+invoice_info['TotalBilled'] = '${}.00'.format(str(int(total_billed)))
 
 document.merge(**invoice_info)
-document.merge_rows('WorkDescription',work_history)
+document.merge_rows('WorkTitle',work_events)
 document.write('reports/Invoice - {}.docx'.format(date.today()))
