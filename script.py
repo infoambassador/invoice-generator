501fe7f18f7dd94e832ba15738a1b74458f7d39c
infoambassador <mlewis0260@gmail.com>
2017-09-08 12:10:48 -0500

    Working demo.

---
+++ script.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import os
+from mailmerge import MailMerge
+from datetime import date
+
+template = 'templates/invoice_template.docx'
+
+document = MailMerge(template)
+
+invoice_info = {
+    'Name': 'John Smith',
+    'StreetAddress': '5555 FakeAddress Drive',
+    'CityStateZip':'Austin, TX 78749',
+    'InvoiceDate': '9/8/2017',
+    'PayeeName': 'Kelly Smith',
+    'PayeeStreetAddress': '5555 FakeAddress Drive',
+    'PayeeCityStateZip': 'Austin, TX 78749',
+    'TotalBilled': '$4000.00'
+}
+
+document.merge(**invoice_info)
+document.write('reports/testoutput.docx')
